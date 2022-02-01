8ece48bab616e40e69a8484da69d85ffe744781a
Mustafa Munawar Anik <anik4good@gmail.com>
2022-02-01 16:20:15 +0600

    added shoot id generator

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -5,6 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"regexp"
+	"strings"
+
 	//"strings"
 	"time"
 	gonanoid "github.com/matoous/go-nanoid"
@@ -71,6 +74,59 @@ func SmsApi(c *fiber.Ctx) error   {
 	}
 
 
+	fmt.Println(len(data.Contacts))
+	//if len(data.Contacts) !=11 {
+	//	return c.Status(400).JSON("Mobile No should be 11 digit")
+	//}
+
+	valid := GetValidPhoneNumber(data.Contacts)
+	if valid == false {
+		return c.Status(400).JSON("Invalid Phone Number")
+	}
+
 	id, _ := gonanoid.Nanoid(13)
+
 	return c.Status(200).JSON("Your SMS is Submitted. ID: "+data.Senderid+"_"+id)
+}
+
+
+
+
+
+
+func GetValidPhoneNumber(number string) bool  {
+	reg := regexp.MustCompilePOSIX("/[^0-9]/")
+	msisdn := reg.ReplaceAllString(number,"")
+
+
+	if len(msisdn) ==11 {
+		if strings.HasPrefix(msisdn,"01") {
+			log.Println("01")
+
+		return  true
+		}
+
+		return false
+
+	}
+
+	if len(msisdn) ==13 {
+		if strings.HasPrefix(msisdn,"8801") {
+
+			return  true
+		}
+
+	}
+
+	if len(msisdn) ==14 {
+		if strings.HasPrefix(msisdn,"+8801") {
+
+			return  true
+		}
+
+	}
+
+
+	return false
+
 }
