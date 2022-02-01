52bac823f825df9d257b8b89e7b2de8192ae4fc8
Mustafa Munawar Anik <anik4good@gmail.com>
2022-02-01 16:25:20 +0600

    added helper function

--- controllers/UserController.go
+++ controllers/UserController.go
@@ -4,9 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"github.com/anik4good/fiber_boilerplate/util"
 	"log"
-	"regexp"
-	"strings"
 
 	//"strings"
 	"time"
@@ -79,7 +78,7 @@ func SmsApi(c *fiber.Ctx) error   {
 	//	return c.Status(400).JSON("Mobile No should be 11 digit")
 	//}
 
-	valid := GetValidPhoneNumber(data.Contacts)
+	valid := util.GetValidPhoneNumberUpdated(data.Contacts)
 	if valid == false {
 		return c.Status(400).JSON("Invalid Phone Number")
 	}
@@ -94,39 +93,4 @@ func SmsApi(c *fiber.Ctx) error   {
 
 
 
-func GetValidPhoneNumber(number string) bool  {
-	reg := regexp.MustCompilePOSIX("/[^0-9]/")
-	msisdn := reg.ReplaceAllString(number,"")
 
-
-	if len(msisdn) ==11 {
-		if strings.HasPrefix(msisdn,"01") {
-			log.Println("01")
-
-		return  true
-		}
-
-		return false
-
-	}
-
-	if len(msisdn) ==13 {
-		if strings.HasPrefix(msisdn,"8801") {
-
-			return  true
-		}
-
-	}
-
-	if len(msisdn) ==14 {
-		if strings.HasPrefix(msisdn,"+8801") {
-
-			return  true
-		}
-
-	}
-
-
-	return false
-
-}
---
+++ util/helper.go
@@ -0,0 +1,85 @@
+package util
+
+import (
+	gonanoid "github.com/matoous/go-nanoid"
+	"log"
+	"regexp"
+	"strings"
+)
+
+func GetTransactionID(prefix string) string {
+	id,_:=gonanoid.Nanoid(13)
+	return prefix+"-"+id
+}
+
+func GetValidPhoneNumber(number string) (string,bool)  {
+	reg := regexp.MustCompilePOSIX("/[^0-9]/")
+	msisdn := reg.ReplaceAllString(number,"")
+
+
+	if len(msisdn) ==11 {
+		if strings.HasPrefix(msisdn,"01") {
+			log.Println("01")
+
+			return "88"+msisdn, true
+		}
+
+		return msisdn,false
+
+	}
+
+	if len(msisdn) ==13 {
+		if strings.HasPrefix(msisdn,"8801") {
+
+			return  msisdn,true
+		}
+
+	}
+
+	return msisdn,false
+
+}
+
+
+
+
+
+
+
+
+func GetValidPhoneNumberUpdated(number string) bool  {
+	reg := regexp.MustCompilePOSIX("/[^0-9]/")
+	msisdn := reg.ReplaceAllString(number,"")
+
+
+	if len(msisdn) ==11 {
+		if strings.HasPrefix(msisdn,"01") {
+			log.Println("01")
+
+			return  true
+		}
+
+		return false
+
+	}
+
+	if len(msisdn) ==13 {
+		if strings.HasPrefix(msisdn,"8801") {
+
+			return  true
+		}
+
+	}
+
+	if len(msisdn) ==14 {
+		if strings.HasPrefix(msisdn,"+8801") {
+
+			return  true
+		}
+
+	}
+
+
+	return false
+
+}
